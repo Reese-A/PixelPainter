c950f4df100773b1f03ba03ab2246960e76dbb10
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-03 19:44:41 -1000

    random colors

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -15,4 +15,45 @@ function createCanvas(num1, num2){
   }
 }
 
-createCanvas(4,4);
+createCanvas(30,30);
+
+
+let emptySpace = document.createElement('h2');
+emptySpace.id = 'blank';
+pixelPainter.appendChild(emptySpace);
+
+
+function createColorGrid(num1, num2){
+  let colorGrid = document.createElement('div');
+  colorGrid.className = 'colors';
+  pixelPainter.appendChild(colorGrid);
+  for (i = 0; i < num1; i++) {
+    let colorRows = document.createElement('div');
+    colorRows.className = 'colorRow';
+    colorGrid.appendChild(colorRows);
+    let colorCount = document.getElementsByClassName('colorRow');
+    for (j = 0; j < num2; j++) {
+      let colorCells = document.createElement('div');
+      colorCells.className = 'colorCell';
+      colorCount[i].appendChild(colorCells);
+    }
+  }
+}
+
+createColorGrid(1,8);
+
+function ran_col() { //function name
+  // var color = '#'; // hexadecimal starting symbol
+  // var letters = ['000000', 'FF0000', '00FF00', '0000FF', 'FFFF00', '00FFFF', 'FF00FF', 'C0C0C0']; //Set your colors here
+  let color = "#" + ((1 << 24) * Math.random() | 0).toString(16);
+  return color;
+}
+
+function setColors(){
+  colorSelect = document.getElementsByClassName('colorCell');
+  for(let i=0;i<colorSelect.length;i++){
+  colorSelect[i].style.background = ran_col();
+  }
+}
+
+setColors();
